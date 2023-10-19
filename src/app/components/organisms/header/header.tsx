e3794724927261374252c198d87d6e1e0e316709
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-19 18:12:13 +0900

    headerのスタイル修正

--- src/app/components/organisms/header/header.tsx
+++ src/app/components/organisms/header/header.tsx
@@ -1,8 +1,18 @@
-import { AppBar } from "@mui/material";
+import { AppBar, IconButton, Toolbar } from "@mui/material";
 import { memo } from "react";
+import MenuIcon from "@mui/icons-material/menu";
 
 export const Header: React.FC = memo(() => {
-  return <AppBar sx={{ position: "sticky" }}>Header</AppBar>;
+  return (
+    <AppBar sx={{ position: "sticky" }}>
+      <Toolbar>
+        <IconButton color="inherit">
+          <MenuIcon />
+        </IconButton>
+        React Lab
+      </Toolbar>
+    </AppBar>
+  );
 });
 
 Header.displayName = "Header";
