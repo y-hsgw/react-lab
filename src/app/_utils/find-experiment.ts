6e7cd82afdd017b77314d8c60fb6668df9c38e85
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-21 19:14:52 +0900

    findExperimentのロジック修正

--- src/app/_utils/find-experiment.ts
+++ src/app/_utils/find-experiment.ts
@@ -2,5 +2,11 @@ import { EXPERIMENTS } from "./experiments";
 
 type ExperimentTitle = (typeof EXPERIMENTS)[number]["title"];
 
-export const findExperiment = (title: ExperimentTitle) =>
-  EXPERIMENTS.find((experiment) => experiment.title === title);
+export const findExperiment = (title: ExperimentTitle) => {
+  const foundExperiments = EXPERIMENTS.find(
+    (experiment) => experiment.title === title
+  );
+  if (!foundExperiments) throw new Error("実験対象が見つかりません");
+
+  return foundExperiments;
+};
