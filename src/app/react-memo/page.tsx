5018e25072bf1ebdbe7daf693d429ef4faf2e570
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-21 19:15:31 +0900

    React.memoのページにメタデータ追加

--- src/app/react-memo/page.tsx
+++ src/app/react-memo/page.tsx
@@ -1,8 +1,30 @@
+import { Typography } from "@mui/material";
+import Link from "next/link";
+import { Metadata } from "next";
+import { findExperiment } from "../_utils/find-experiment";
+
+const foundExperiment = findExperiment("React.memo");
+
+export const metadata: Metadata = {
+  title: `React Lab - ${foundExperiment.title} -`,
+  description: foundExperiment.description,
+};
+
 export default function ReactMemo() {
   return (
-    <main>
-      memo を使うことで、props
-      が変更されていない場合にコンポーネントの再レンダーをスキップできます
-    </main>
+    <>
+      <Typography component="h2" variant="h5" fontWeight="bold">
+        {foundExperiment.title}
+      </Typography>
+      <Typography>
+        memo を使うことで、props
+        が変更されていない場合にコンポーネントの再レンダーをスキップできます。
+      </Typography>
+      <Typography>
+        <Link href="https://ja.react.dev/reference/react/memo" target="_">
+          https://ja.react.dev/reference/react/memo
+        </Link>
+      </Typography>
+    </>
   );
 }
