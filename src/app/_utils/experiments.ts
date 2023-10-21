52e9c7891ac6e78b9c4beaf1febe3830d6c7b245
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-21 18:16:05 +0900

    EXPERIMENTSの作成

---
+++ src/app/_utils/experiments.ts
@@ -0,0 +1,15 @@
+import { Experiment } from "@/app/_types";
+
+export const EXPERIMENTS: Experiment[] = [
+  {
+    title: "React.memo",
+    description:
+      "コンポーネントの再レンダリングをスキップすることができるReact.Memoの使用法について",
+    path: "/react-memo",
+  },
+  {
+    title: "useState",
+    description: "ステート",
+    path: "/use-state",
+  },
+];
