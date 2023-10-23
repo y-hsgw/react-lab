ee32e921b66cde30e570f862627a432d78f27490
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-23 22:34:02 +0900

    memoページのスタイリング

--- src/app/react-memo/page.tsx
+++ src/app/react-memo/page.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import Link from "next/link";
 import { Metadata } from "next";
 import { findExperiment } from "../_utils/find-experiment";
@@ -22,23 +22,29 @@ export const metadata: Metadata = {
 
 export default function ReactMemo() {
   return (
-    <>
-      <Typography component="h2" variant="h5" fontWeight="bold">
-        {foundExperiment.title}
-      </Typography>
-      <Typography>
-        memo を使うことで、props
-        が変更されていない場合にコンポーネントの再レンダーをスキップできます。
-      </Typography>
-      <Typography>
-        <Link href="https://ja.react.dev/reference/react/memo" target="_">
-          https://ja.react.dev/reference/react/memo
-        </Link>
-      </Typography>
-      <Typography fontWeight="bold">React.memoを使用しない場合</Typography>
-      <ExperimentSandpack files={noMemorizedFiles} />
-      <Typography fontWeight="bold">React.memoを使用する場合</Typography>
-      <ExperimentSandpack files={memorizedFiles} />
-    </>
+    <Stack gap={2}>
+      <div>
+        <Typography component="h2" variant="h5" fontWeight="bold">
+          {foundExperiment.title}
+        </Typography>
+        <Typography>
+          memo を使うことで、props
+          が変更されていない場合にコンポーネントの再レンダーをスキップできます。
+        </Typography>
+        <Typography>
+          <Link href="https://ja.react.dev/reference/react/memo" target="_">
+            https://ja.react.dev/reference/react/memo
+          </Link>
+        </Typography>
+      </div>
+      <div>
+        <Typography fontWeight="bold">React.memoを使用しない場合</Typography>
+        <ExperimentSandpack files={noMemorizedFiles} />
+      </div>
+      <div>
+        <Typography fontWeight="bold">React.memoを使用する場合</Typography>
+        <ExperimentSandpack files={memorizedFiles} />
+      </div>
+    </Stack>
   );
 }
