93d01b746854cb788e08145d7f6a75ef2ef5ba8f
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-25 22:28:28 +0900

    React.memoのスタイリングとテキスト追加

--- src/app/react-memo/page.tsx
+++ src/app/react-memo/page.tsx
@@ -24,25 +24,37 @@ export default function ReactMemo() {
   return (
     <Stack gap={2}>
       <div>
-        <Typography component="h2" variant="h5" fontWeight="bold">
+        <Typography component="h2" variant="h4" fontWeight="bold">
           {foundExperiment.title}
         </Typography>
-        <Typography>
-          memo を使うことで、props
-          が変更されていない場合にコンポーネントの再レンダーをスキップできます。
-        </Typography>
-        <Typography>
+        <Typography my={1}>
+          memoを使うことで、propsが変更されていない場合にコンポーネントの再レンダーをスキップできます。
+          <br />
+          コンポーネントがメモ化されていても、コンポーネント自体の状態が変化すると再レンダリングされます。
+          <strong>
+            メモ化は、親からコンポーネントに渡される props にのみ関係します。
+          </strong>
           <Link href="https://ja.react.dev/reference/react/memo" target="_">
             https://ja.react.dev/reference/react/memo
           </Link>
         </Typography>
       </div>
       <div>
-        <Typography fontWeight="bold">React.memoを使用しない場合</Typography>
+        <Typography component="h3" variant="h6" fontWeight="bold">
+          React.memoを使用しない場合
+        </Typography>
+        <Typography my={1}>
+          子コンポーネント（Greeting）へのpropsの値が変わらない場合、子コンポーネントを再レンダーする。
+        </Typography>
         <ExperimentSandpack files={noMemorizedFiles} />
       </div>
       <div>
-        <Typography fontWeight="bold">React.memoを使用する場合</Typography>
+        <Typography component="h3" variant="h6" fontWeight="bold">
+          React.memoを使用する場合
+        </Typography>
+        <Typography my={1}>
+          子コンポーネント（Greeting）へのpropsの値が変わらない場合、子コンポーネントは再レンダーされない。
+        </Typography>
         <ExperimentSandpack files={memorizedFiles} />
       </div>
     </Stack>
