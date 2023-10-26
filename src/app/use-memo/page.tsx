6dd9fc49a28980d8b5598466af1683fefb0f2b73
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-26 23:55:41 +0900

    sandboxに反映

--- src/app/use-memo/page.tsx
+++ src/app/use-memo/page.tsx
@@ -1,8 +1,15 @@
 import { Stack, Typography } from "@mui/material";
 import { findExperiment } from "../_utils/find-experiment";
 import { Metadata } from "next";
+import Link from "next/link";
+import { ExperimentSandpack } from "../_components/organisms/experiment-sandpack";
+import { readFileInCodeSandbox } from "../_utils/readFileInCodeSandbox";
 
 const foundExperiment = findExperiment("useMemo");
+
+const basicUseMemoFiles = readFileInCodeSandbox(
+  "src/app/use-memo/_components/basic-use-memo.tsx"
+);
 
 export const metadata: Metadata = {
   title: `React Lab - ${foundExperiment.title} -`,
@@ -16,6 +23,16 @@ export default function UseMemo() {
         <Typography component="h2" variant="h4" fontWeight="bold">
           {foundExperiment.title}
         </Typography>
+        <Typography my={1}>
+          useMemoは、再レンダリング間の計算結果をキャッシュできるReactフックです。
+          <br />
+          <Link href="https://react.dev/reference/react/useMemo" target="_">
+            https://react.dev/reference/react/useMemo
+          </Link>
+        </Typography>
+      </div>
+      <div>
+        <ExperimentSandpack files={basicUseMemoFiles} />
       </div>
     </Stack>
   );
