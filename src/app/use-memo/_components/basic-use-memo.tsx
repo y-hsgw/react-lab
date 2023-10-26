577bd730dd39c13b3586f368d49ad0ddd32b9f6a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-26 23:55:23 +0900

    BasicUseMemoでuseMemo使用するよう実装

--- src/app/use-memo/_components/basic-use-memo.tsx
+++ src/app/use-memo/_components/basic-use-memo.tsx
@@ -1,28 +1,46 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 interface Props {
-  todos: string[];
+  todoList: string[];
 }
 
-const TodoList: React.FC<Props> = ({ todos }) => {
-  const filteredTodos = useMemo(
-    () => todos.filter((todo) => todo === ""),
-    [todos]
-  );
+const TodoList: React.FC<Props> = ({ todoList }) => {
+  const [value, setValue] = useState("");
+
+  console.time("filteredTodoList");
+  const filteredTodoList = useMemo(() => {
+    console.log("filteredTodoList");
+    return todoList.filter((todo) => todo !== "");
+  }, [todoList]);
+  console.timeEnd("filteredTodoList");
+
+  console.time("noMemorizedFilteredTodoList");
+  const noMemorizedFilteredTodoList = () => {
+    console.log("noMemorizedFilteredTodoList");
+    return todoList.filter((todo) => todo !== "");
+  };
+  console.timeEnd("noMemorizedFilteredTodoList");
 
   return (
     <div>
-      {filteredTodos.map((todo) => (
-        <p key={todo}>{todo}</p>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      {filteredTodoList.map((todo, i) => (
+        <p key={i}>{todo}</p>
+      ))}
+      {noMemorizedFilteredTodoList().map((todo, i) => (
+        <p key={i}>{todo}</p>
       ))}
     </div>
   );
 };
 
 const BasicUseMemo: React.FC = () => {
+  const emptyStrings = new Array<string>(500).fill("");
+  const textStrings = new Array<string>(1000).fill("text");
+  const dummyTodoList = emptyStrings.concat(textStrings);
   return (
     <div>
-      <TodoList todos={[]} />
+      <TodoList todoList={dummyTodoList} />
     </div>
   );
 };
