a1706914eb8f831ebaaa9e0b01cccfeaedaf8c39
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-28 19:25:17 +0900

    InactiveUseCallbackの作成

---
+++ src/app/use-callback/_components/inactive-use-callback.tsx
@@ -0,0 +1,50 @@
+import { memo, useState } from "react";
+
+interface Props {
+  onClick: () => void;
+}
+
+const Button: React.FC<Props> = memo(({ onClick }) => {
+  console.log(`Button was rendered at ${new Date().toLocaleTimeString()}`);
+
+  return <button onClick={onClick}>Hello</button>;
+});
+
+const Form: React.FC = () => {
+  const [name, setName] = useState("");
+
+  const handleClick = () => {
+    window.alert(`Hello! ${name}`);
+  };
+
+  return (
+    <div>
+      <label>名前</label>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <Button onClick={handleClick} />
+    </div>
+  );
+};
+
+const InactiveUseCallback: React.FC = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  return (
+    <div
+      style={{
+        backgroundColor: isDark ? "black" : "initial",
+        color: isDark ? "white" : "initial",
+      }}
+    >
+      <label>テーマ</label>
+      <input
+        type="checkbox"
+        checked={isDark}
+        onChange={(e) => setIsDark(e.target.checked)}
+      />
+      <Form />
+    </div>
+  );
+};
+
+export default InactiveUseCallback;
