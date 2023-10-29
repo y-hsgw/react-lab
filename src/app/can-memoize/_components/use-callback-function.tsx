8128c4448f9247e296c23642fcdfe3f970abed22
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-29 16:19:52 +0900

    UseCallbackFunctionの作成

---
+++ src/app/can-memoize/_components/use-callback-function.tsx
@@ -0,0 +1,38 @@
+import { memo, useCallback, useState } from "react";
+
+interface Props {
+  onClick: () => void;
+}
+
+const Button: React.FC<Props> = memo(({ onClick }) => {
+  console.log(`Button was rendered at ${new Date().toLocaleTimeString()}`);
+
+  return <button onClick={onClick}>Click!!</button>;
+});
+
+const UseCallbackFunction: React.FC = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  const func = useCallback(() => {
+    console.log("called func");
+  }, []);
+
+  return (
+    <div
+      style={{
+        backgroundColor: isDark ? "black" : "initial",
+        color: isDark ? "white" : "initial",
+      }}
+    >
+      <label>テーマ</label>
+      <input
+        type="checkbox"
+        checked={isDark}
+        onChange={(e) => setIsDark(e.target.checked)}
+      />
+      <Button onClick={func} />
+    </div>
+  );
+};
+
+export default UseCallbackFunction;
