430e3d189c5da6619b47284a7bba02c610b73567
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-30 19:05:18 +0900

    DependencyArrayの実装

---
+++ src/app/use-effect/_components/dependency-array.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface Props {
+  name: string;
+}
+
+const Greeting: React.FC<Props> = ({ name }) => {
+  useEffect(() => {
+    console.log(`Hello ${name}`);
+  }, [name]);
+
+  return <p>Hello {name}</p>;
+};
+
+const DependencyArray: React.FC = () => {
+  const [isDark, setIsDark] = useState(false);
+  const [name, setName] = useState("");
+
+  return (
+    <div
+      style={{
+        backgroundColor: isDark ? "black" : "initial",
+        color: isDark ? "white" : "initial",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        marginTop: 8,
+      }}
+    >
+      <label>テーマ</label>
+      <input
+        type="checkbox"
+        checked={isDark}
+        onChange={(e) => setIsDark(e.target.checked)}
+      />
+      <label>名前</label>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <Greeting name={name} />
+    </div>
+  );
+};
+
+export default DependencyArray;
