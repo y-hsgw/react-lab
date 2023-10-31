630720007e42d00004b2409fff6ac17ed51b629a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-31 20:52:19 +0900

    propsが変更されたときに一部のstateを調整するケースを追記

--- src/app/unnecessary-use-effect-cases/page.tsx
+++ src/app/unnecessary-use-effect-cases/page.tsx
@@ -15,6 +15,10 @@ const resetStateOnPropsChangeWithKey = readFileInCodeSandbox(
   "src/app/unnecessary-use-effect-cases/_components/reset-state-on-props-change-with-key.tsx"
 );
 
+const partialResetStateOnPropsChangeWithKey = readFileInCodeSandbox(
+  "src/app/unnecessary-use-effect-cases/_components/partial-reset-state-on-props-change-with-use-effect.tsx"
+);
+
 export const metadata: Metadata = {
   title: `React Lab - ${experiment.title} -`,
   description: experiment.description,
@@ -58,6 +62,24 @@ export default function UnnecessaryUseEffectCases() {
           属性を使用することでstateをリセットすることができます。
         </Typography>
         <ExperimentSandpack files={resetStateOnPropsChangeWithKey} />
+      </div>
+      <div>
+        <Typography variant="h2">
+          propsが変更されたときに一部のstateを調整する
+        </Typography>
+        <Typography>
+          propsの値が変更されたときに全部のstateではなく一部のみをリセットするケースについて比較します。
+          <br />
+          ex）追加したメンバーの中から任意のメンバーを選択。その後、再度メンバーを追加すると選択がリセットされるが、グループ名はリセットされない仕様を例とします。
+        </Typography>
+      </div>
+      <div>
+        <Typography variant="h3">useEffectを使用する場合</Typography>
+        <Typography>
+          useEffectを使用してpropsで受け取った値を依存配列に指定して、一部のstateをリセットします。
+          <br />
+        </Typography>
+        <ExperimentSandpack files={partialResetStateOnPropsChangeWithKey} />
       </div>
     </Stack>
   );
