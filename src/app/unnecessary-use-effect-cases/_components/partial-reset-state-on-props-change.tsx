c70457632103cb180f9795c31a18e032d7a14165
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-01 22:32:21 +0900

    PartialResetStateOnPropsChangeの実装

---
+++ src/app/unnecessary-use-effect-cases/_components/partial-reset-state-on-props-change.tsx
@@ -0,0 +1,84 @@
+import { ChangeEvent, memo, useState } from "react";
+
+type Item = {
+  id: number;
+  name: string;
+};
+
+interface Props {
+  items: Item[];
+}
+
+const List: React.FC<Props> = memo(({ items }) => {
+  const [name, setName] = useState("");
+  const [selectedId, setSelectedId] = useState<number>();
+  const selectionItem = items.find((item) => item.id === selectedId);
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleRadioChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSelectedId(Number(e.target.value));
+  };
+
+  return (
+    <div>
+      <form>
+        <label>
+          あなたの名前
+          <input value={name} onChange={handleNameChange} />
+        </label>
+      </form>
+      {selectionItem ? (
+        <p>選択中：{selectionItem.name}</p>
+      ) : (
+        <p>お気に入りを選択してください</p>
+      )}
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        {items.map((item) => (
+          <label key={item.id}>
+            {item.name}
+            <input
+              type="radio"
+              value={item.id}
+              checked={item.id === selectionItem?.id}
+              onChange={handleRadioChange}
+            />
+          </label>
+        ))}
+      </form>
+    </div>
+  );
+});
+
+const PartialResetStateOnPropsChange: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  const handleAnimalClick = () => {
+    setItems([
+      { id: 1, name: "ネコ" },
+      { id: 2, name: "イヌ" },
+      { id: 3, name: "ウサギ" },
+    ]);
+  };
+
+  const handleFoodClick = () => {
+    setItems([
+      { id: 10, name: "寿司" },
+      { id: 20, name: "焼き肉" },
+      { id: 30, name: "ラーメン" },
+    ]);
+  };
+
+  return (
+    <div>
+      <p>カテゴリを選択してください。</p>
+      <button onClick={handleAnimalClick}>動物</button>
+      <button onClick={handleFoodClick}>食べ物</button>
+      <List items={items} />
+    </div>
+  );
+};
+
+export default PartialResetStateOnPropsChange;
