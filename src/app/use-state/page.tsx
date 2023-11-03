a045b60372903f000adc3680a1777793f74853c9
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-03 17:14:26 +0900

    前の状態を基づいて状態を更新するケースの追記

--- src/app/use-state/page.tsx
+++ src/app/use-state/page.tsx
@@ -11,6 +11,14 @@ const basicUseStateFile = readFileInCodeSandbox(
   "src/app/use-state/_components/basic-use-state.tsx"
 );
 
+const prevStateFile = readFileInCodeSandbox(
+  "src/app/use-state/_components/prev-state.tsx"
+);
+
+const nextStateFile = readFileInCodeSandbox(
+  "src/app/use-state/_components/next-state.tsx"
+);
+
 export const metadata: Metadata = {
   title: `React Lab - ${experiment.title} -`,
   description: experiment.description,
@@ -40,6 +48,28 @@ export default function UseState() {
           関数を実行して<code>name</code>の状態を更新しています。
         </Typography>
         <ExperimentSandpack files={basicUseStateFile} />
+      </div>
+      <div>
+        <Typography variant="h2">前の状態に基づいて状態を更新する</Typography>
+        <Typography>
+          set関数の引数にアップデーター関数を指定すると前の状態を基に更新ができるようになります。
+          ex）「+3」ボタンをクリックしたらカウンターが3つ増加する
+        </Typography>
+      </div>
+      <div>
+        <Typography variant="h3">アップデーター関数を使用する場合</Typography>
+        <Typography>
+          前の状態のcountを基に1つ増加するようになっています。
+        </Typography>
+        <ExperimentSandpack files={prevStateFile} />
+      </div>
+      <div>
+        <Typography variant="h3">次の状態を直接使用する場合</Typography>
+        <Typography>
+          <code>set</code>関数を実行しても、すでに実行されているコードの
+          <code>count</code>状態は更新されないです。
+        </Typography>
+        <ExperimentSandpack files={nextStateFile} />
       </div>
     </Stack>
   );
