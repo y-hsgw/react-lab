3ce2a4672f9882760b8a3d381c7e007a45b3a748
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-03 16:42:54 +0900

    基本的な使用法の追記

--- src/app/use-state/page.tsx
+++ src/app/use-state/page.tsx
@@ -2,15 +2,21 @@ import { Stack, Typography } from "@mui/material";
 import { getExperiment } from "../_utils/get-experiment";
 import { Metadata } from "next";
 import Link from "next/link";
+import { ExperimentSandpack } from "../_components/organisms/experiment-sandpack";
+import { readFileInCodeSandbox } from "../_utils/readFileInCodeSandbox";
 
 const experiment = getExperiment("useState");
+
+const basicUseStateFile = readFileInCodeSandbox(
+  "src/app/use-state/_components/basic-use-state.tsx"
+);
 
 export const metadata: Metadata = {
   title: `React Lab - ${experiment.title} -`,
   description: experiment.description,
 };
 
-export default function UseMemo() {
+export default function UseState() {
   return (
     <Stack gap={2}>
       <div>
@@ -22,6 +28,18 @@ export default function UseMemo() {
             https://react.dev/reference/react/useState
           </Link>
         </Typography>
+      </div>
+      <div>
+        <Typography variant="h2">基本的な使用法</Typography>
+        <Typography>
+          戻り値の配列の最初の要素が現在の状態。2つ目の要素が要素が状態を更新する
+          <code>set</code>関数。
+          <br />
+          ex）<code>name</code>を状態として保持し、
+          <code>onChange</code>イベントが実行するたびに<code>set</code>
+          関数を実行して<code>name</code>の状態を更新しています。
+        </Typography>
+        <ExperimentSandpack files={basicUseStateFile} />
       </div>
     </Stack>
   );
