bcbc685a385d9964f1fc233d3c5837dc51f3b519
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-03 14:49:56 +0900

    BrowserAPIの実装

---
+++ src/app/use-sync-external-store/_components/browser-api.tsx
@@ -0,0 +1,21 @@
+import { useSyncExternalStore } from "react";
+
+const subscribe = (callback: () => void) => {
+  window.addEventListener("online", callback);
+  window.addEventListener("offline", callback);
+  return () => {
+    window.removeEventListener("online", callback);
+    window.removeEventListener("offline", callback);
+  };
+};
+
+const getSnapshot = () => {
+  return navigator.onLine;
+};
+
+const BrowserApi: React.FC = () => {
+  const isOnline = useSyncExternalStore(subscribe, getSnapshot);
+  return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
+};
+
+export default BrowserApi;
