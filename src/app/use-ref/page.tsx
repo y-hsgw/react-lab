e9020c6529a01ced3c27e0ec00a732b6002a012d
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-04 12:05:21 +0900

    refを使用した場合のケース追記

--- src/app/use-ref/page.tsx
+++ src/app/use-ref/page.tsx
@@ -2,8 +2,18 @@ import { Stack, Typography } from "@mui/material";
 import { getExperiment } from "../_utils/get-experiment";
 import { Metadata } from "next";
 import Link from "next/link";
+import { ExperimentSandpack } from "../_components/organisms/experiment-sandpack";
+import { readFileInCodeSandbox } from "../_utils/readFileInCodeSandbox";
 
-const experiment = getExperiment("useSyncExternalStore");
+const experiment = getExperiment("useRef");
+
+const controlDOMFile = readFileInCodeSandbox(
+  "src/app/use-ref/_components/control-dom.tsx"
+);
+
+const forwardRefFile = readFileInCodeSandbox(
+  "src/app/use-ref/_components/forward-ref.tsx"
+);
 
 export const metadata: Metadata = {
   title: `React Lab - ${experiment.title} -`,
@@ -22,6 +32,27 @@ export default function UseRef() {
             https://react.dev/reference/react/useRef
           </Link>
         </Typography>
+      </div>
+      <div>
+        <Typography variant="h2">refを使用したDOMの操作</Typography>
+        <Typography>
+          DOMを操作するために<code>ref</code>を使用するのが特に一般的です。
+          <br /> ex）<code>ref</code>で取得したDOMに<code>scrollIntoView</code>
+          を実行してスクロールを表現しています。
+        </Typography>
+        <ExperimentSandpack files={controlDOMFile} />
+      </div>
+      <div>
+        <Typography variant="h2">
+          カスタムコンポーネントへrefを渡す方法
+        </Typography>
+        <Typography>
+          <Link href="https://react.dev/reference/react/forwardRef" target="_">
+            forwardRef
+          </Link>
+          でラップする
+        </Typography>
+        <ExperimentSandpack files={forwardRefFile} />
       </div>
     </Stack>
   );
