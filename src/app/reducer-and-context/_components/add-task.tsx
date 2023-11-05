f814d967edf6440c5fe08e7dffc4ab8171bd54b1
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-05 18:41:21 +0900

    空文字のタスクは追加できないようにする

--- src/app/reducer-and-context/_components/add-task.tsx
+++ src/app/reducer-and-context/_components/add-task.tsx
@@ -10,6 +10,8 @@ const AddTask: React.FC = () => {
   };
 
   const handleClick = () => {
+    if (!text) return;
+
     setText("");
     dispatch({ type: "added", id: new Date().getTime(), name: text });
   };
