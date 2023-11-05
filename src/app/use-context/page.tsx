5104823d197469d60d5614ab60783249d980f4c1
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-05 15:56:27 +0900

    useContextの基本的な使用法の追記

--- src/app/use-context/page.tsx
+++ src/app/use-context/page.tsx
@@ -2,8 +2,14 @@ import { Stack, Typography } from "@mui/material";
 import { getExperiment } from "../_utils/get-experiment";
 import { Metadata } from "next";
 import Link from "next/link";
+import { readFileInCodeSandbox } from "../_utils/readFileInCodeSandbox";
+import { ExperimentSandpack } from "../_components/organisms/experiment-sandpack";
 
 const experiment = getExperiment("useContext");
+
+const basicUseContextFile = readFileInCodeSandbox(
+  "src/app/use-context/_components/basic-use-context.tsx"
+);
 
 export const metadata: Metadata = {
   title: `React Lab - ${experiment.title} -`,
@@ -22,6 +28,17 @@ export default function UseContext() {
             https://react.dev/reference/react/useContext
           </Link>
         </Typography>
+      </div>
+      <div>
+        <Typography variant="h2">基本的な使用法</Typography>
+        <Typography>
+          コンポーネントのトップレベルで呼び出してコンテキストを読み取り、サブスクライブします。
+          <br />
+          <code>Button</code>
+          コンポーネントでバケツリレーせず、<code>theme</code>
+          状態を呼び出すことができます。
+        </Typography>
+        <ExperimentSandpack files={basicUseContextFile} />
       </div>
     </Stack>
   );
