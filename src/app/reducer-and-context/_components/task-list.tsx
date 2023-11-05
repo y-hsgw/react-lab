a63b62d688b94af490af49ad493e132d4908914a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-05 18:35:15 +0900

    TaskListの実装

---
+++ src/app/reducer-and-context/_components/task-list.tsx
@@ -0,0 +1,83 @@
+import React, { ChangeEvent, useState } from "react";
+import { Task, useTasks, useTasksDispatch } from "./tasks-context";
+
+interface Props {
+  task: Task;
+}
+
+const TaskListItem: React.FC<Props> = ({ task }) => {
+  const dispatch = useTasksDispatch();
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleDoneCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: "changed",
+      task: {
+        ...task,
+        done: e.target.checked,
+      },
+    });
+  };
+
+  const handleTaskNameInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: "changed",
+      task: {
+        ...task,
+        name: e.target.value,
+      },
+    });
+  };
+
+  const handleSaveButtonClick = () => {
+    setIsEditing(false);
+  };
+
+  const handleEditButtonClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleDeleteButtonClick = () => {
+    dispatch({ type: "deleted", id: task.id });
+  };
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={task.done}
+        onChange={handleDoneCheckboxChange}
+      />
+
+      {isEditing ? (
+        <>
+          <input value={task.name} onChange={handleTaskNameInputChange} />
+          <button onClick={handleSaveButtonClick}>Save</button>
+        </>
+      ) : (
+        <>
+          {task.name}
+          <button onClick={handleEditButtonClick}>Edit</button>
+        </>
+      )}
+
+      <button onClick={handleDeleteButtonClick}>Delete</button>
+    </label>
+  );
+};
+
+const TaskList: React.FC = () => {
+  const tasks = useTasks();
+
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <TaskListItem task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TaskList;
