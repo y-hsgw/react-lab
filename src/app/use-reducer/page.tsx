25b969a17a9e92c48eae9642b2e4ea36bf2a47dc
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-05 16:55:12 +0900

    基本的な使用法追記

--- src/app/use-reducer/page.tsx
+++ src/app/use-reducer/page.tsx
@@ -2,8 +2,14 @@ import { Stack, Typography } from "@mui/material";
 import { getExperiment } from "../_utils/get-experiment";
 import { Metadata } from "next";
 import Link from "next/link";
+import { ExperimentSandpack } from "../_components/organisms/experiment-sandpack";
+import { readFileInCodeSandbox } from "../_utils/readFileInCodeSandbox";
 
 const experiment = getExperiment("useReducer");
+
+const basicUseReducerFile = readFileInCodeSandbox(
+  "src/app/use-reducer/_components/basic-use-reducer.tsx"
+);
 
 export const metadata: Metadata = {
   title: `React Lab - ${experiment.title} -`,
@@ -22,6 +28,15 @@ export default function UseReducer() {
             https://react.dev/reference/react/useReducer
           </Link>
         </Typography>
+      </div>
+      <div>
+        <Typography variant="h2">基本的な使用法</Typography>
+        <Typography>
+          コンポーネントのトップレベルで呼び出して、リデューサーを使用して状態を管理します。
+          <br />
+          ex）todoリストの配列を管理しています。
+        </Typography>
+        <ExperimentSandpack files={basicUseReducerFile} />
       </div>
     </Stack>
   );
