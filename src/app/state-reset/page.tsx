405b6646b03b63ac809a8d62cca9ff6b2ca9c7d5
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-11 18:00:37 +0900

    keyを使用して状態をリセットするケースの追加

--- src/app/state-reset/page.tsx
+++ src/app/state-reset/page.tsx
@@ -1,8 +1,14 @@
 import { Stack, Typography } from "@mui/material";
 import { getExperiment } from "../_utils/get-experiment";
 import { Metadata } from "next";
+import { readFileInCodeSandbox } from "../_utils/readFileInCodeSandbox";
+import { ExperimentSandpack } from "../_components/organisms/experiment-sandpack";
 
 const experiment = getExperiment("状態のリセット");
+
+const keyResetFile = readFileInCodeSandbox(
+  "src/app/state-reset/_components/key-reset.tsx"
+);
 
 export const metadata: Metadata = {
   title: `React Lab - ${experiment.title} -`,
@@ -15,6 +21,13 @@ export default function StateReset() {
       <div>
         <Typography variant="h1">{experiment.title}</Typography>
         <Typography my={1}>状態のリセット方法についてまとめます。</Typography>
+      </div>
+      <div>
+        <Typography variant="h2">keyを使用して状態をリセットする</Typography>
+        <Typography>
+          異なるKeyを渡すと渡されたコンポーネントの状態はリセットされます。
+        </Typography>
+        <ExperimentSandpack files={keyResetFile} />
       </div>
     </Stack>
   );
