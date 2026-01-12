e4b0595612073b60d784c305542b3d28c0a109b6
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2026-01-12 21:18:57 +0900

    パッケージ更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { CssBaseline, GlobalStyles, Stack } from "@mui/material";
 import type { Metadata } from "next";
 import { Header } from "@/app/_components/organisms/header";
 import { ThemeProvider } from "./_providers/theme-provider";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 export const metadata: Metadata = {
   title: "React Lab",
@@ -16,27 +17,29 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <ThemeProvider>
-          <CssBaseline />
-          <GlobalStyles
-            styles={{
-              body: {
-                display: "flex",
-                flexDirection: "column",
-                minHeight: "100vh",
-              },
-              code: {
-                padding: "1px 0.25rem",
-                backgroundColor: "rgb(153 161 179/0.1)",
-                borderRadius: 3,
-              },
-            }}
-          />
-          <Header />
-          <Stack component="main" p={2} flex={1}>
-            {children}
-          </Stack>
-        </ThemeProvider>
+        <AppRouterCacheProvider>
+          <ThemeProvider>
+            <CssBaseline />
+            <GlobalStyles
+              styles={{
+                body: {
+                  display: "flex",
+                  flexDirection: "column",
+                  minHeight: "100vh",
+                },
+                code: {
+                  padding: "1px 0.25rem",
+                  backgroundColor: "rgb(153 161 179/0.1)",
+                  borderRadius: 3,
+                },
+              }}
+            />
+            <Header />
+            <Stack component="main" p={2} flex={1}>
+              {children}
+            </Stack>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
